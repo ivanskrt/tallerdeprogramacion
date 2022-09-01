dc89a39c0fbb75cbdc07a77d91be03bef863f421
ivanskrt <74741962+ivanskrt@users.noreply.github.com>
2022-08-31 23:37:56 -0300

    Add files via upload

---
+++ ejerciciosFeli/P1E5.pas
@@ -0,0 +1,21 @@
+{
+Implementar un módulo que realice una búsqueda dicotómica en un vector, utilizando el siguiente encabezado:
+   	Procedure busquedaDicotomica (v: vector; ini,fin: indice; dato:integer; var pos: indice); 
+
+Nota: El parámetro “pos” debe retornar la posición del dato o -1 si el dato no se encuentra en el vector.
+   
+}
+
+
+program untitled;
+
+uses crt;
+var i : byte;
+
+Procedure busquedaDicotomica (v: vector; ini,fin: indice; dato:integer; var pos: indice); 
+
+BEGIN
+	
+	
+END.
+
